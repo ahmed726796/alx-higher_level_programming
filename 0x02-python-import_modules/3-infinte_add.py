899ca1d9c598931b1d998920df93cf935ba4de74
ahmed726796 <ahmedhassan726796@gmail.com>
2023-09-07 13:57:43 -0700

    updated project

--- 0x02-python-import_modules/3-infinte_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    length = len(sys.argv) - 1
-    Sum = 0
-    for i in range(length):
-        Sum += int(sys.argv[i + 1])
-    print("{}".format(Sum))
